4f2f4a358cd8b1f43031105f01d766d7c7bcef0f
unknown <tao32040@gmail.com>
2019-08-15 13:15:18 +0700

    edit nav in holiday page

--- admin/menu.php
+++ admin/menu.php
@@ -4,7 +4,7 @@
       <a class="navbar-brand" href="#">ADMIN</a>
       <a class="nav-link " id="home-tab" data-toggle="tab" href="../" role="tab" aria-controls="home" aria-selected="true">หน้าแรก</a>
       <a class="nav-link" id="profile-tab" data-toggle="tab" href="../test.php" role="tab" aria-controls="profile" aria-selected="false">เพิ่มQuery</a>
-      <a class="nav-link" id="contact-tab" data-toggle="tab" href="holiday.php" role="tab" aria-controls="contact" aria-selected="false">Contact</a>
+      <a class="nav-link" id="contact-tab" data-toggle="tab" href="holiday.php" role="tab" aria-controls="contact" aria-selected="false">วันหยุด HOSxp</a>
     </ul> ' ?>
     <? echo "<span  class='new_sql mr-4' >" . 'ผู้ใช้งาน : ' . $_SESSION['fname'] . ' ' . $_SESSION['lname'].' STATUS: ';
      if ($_SESSION['status'] == '1') { echo "admin  </span>";  } ?>
