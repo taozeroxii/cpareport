b1102831266995a4a51091cede4a618ab6826ef4
unknown <tao32040@gmail.com>
2019-08-16 15:00:40 +0700

    เพิ่มไฟลใหม่แก้ไขบางส่วน

--- admin/menu.php
+++ admin/menu.php
@@ -5,10 +5,10 @@
       <a class="nav-link " id="home-tab" data-toggle="tab" href="index.php" role="tab" aria-controls="home" aria-selected="true">หน้าแรก</a>
       <a class="nav-link" id="profile-tab" data-toggle="tab" href="addquery.php" role="tab" aria-controls="profile" aria-selected="false">เพิ่มQuery</a>
       <a class="nav-link" id="contact-tab" data-toggle="tab" href="holiday.php" role="tab" aria-controls="contact" aria-selected="false">วันหยุด HOSxp</a>
-      <a class="nav-link" id="contact-tab" data-toggle="tab" href="fixHosxp.php" role="tab" aria-controls="contact" aria-selected="false">รวมปัญหาHOSxp </a>
+      <a class="nav-link" id="contact-tab" data-toggle="tab" href="fixHosxp.php" role="tab" aria-controls="contact" aria-selected="false">รวมปัญหาต่างๆ </a>
     </ul> ' ?>
     <? echo "<span  class='new_sql mr-4' >" . 'ผู้ใช้งาน : ' . $_SESSION['fname'] . ' ' . $_SESSION['lname'].' STATUS: ';
-     if ($_SESSION['status'] == '1') { echo "admin  </span>";  } ?>
+     if ($_SESSION['status']) { echo "admin  </span>";  } ?>
     <? echo '<a href="../logout.php" ><button class="btn-secondary btn-sm " >ออกจากระบบ</button> </a>
   </nav>';?>
 
