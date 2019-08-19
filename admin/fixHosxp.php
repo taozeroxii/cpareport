5a4ba47afd2743796783347a15cfcc5dd18d18f8
eaktamp <eaktamp@gmail.com>
2019-08-19 14:12:02 +0700

    ttt

--- admin/fixHosxp.php
+++ admin/fixHosxp.php
@@ -1,124 +1,86 @@
 <?php session_start(); ?>
-<?php
-include("config.inc.php");
-date_default_timezone_set('asia/bangkok');
-function thaiDate($datetime)
-{
-  if (!is_null($datetime)) {
-    list($date, $time) = split('T', $datetime);
-    list($Y, $m, $d) = split('-', $date);
-    $Y = $Y + 543;
-    switch ($m) {
-      case "01": $m = "ม.ค.";
-        break;
-      case "02":$m = "ก.พ.";
-        break;
-      case "03":$m = "มี.ค.";
-        break;
-      case "04":$m = "เม.ย.";
-        break;
-      case "05": $m = "พ.ค.";
-        break;
-      case "06": $m = "มิ.ย.";
-        break;
-      case "07": $m = "ก.ค.";
-        break;
-      case "08": $m = "ส.ค.";
-        break;
-      case "09": $m = "ก.ย.";
-        break;
-      case "10": $m = "ต.ค.";
-        break;
-      case "11": $m = "พ.ย.";
-        break;
-      case "12":  $m = "ธ.ค.";
-        break;
-    }
-    return $d . " " . $m . " " . $Y . "";
-  }
-  return "";
-}
-$sqlr = " SELECT DISTINCT b.loginname,a.computername
-,date(a.ksklogintime) as cdate
-,to_char(a.ksklogintime, 'HH24:MI:SS') as ctime
-,a.department,a.ip_address,b.name
-FROM onlineuser as a
-INNER JOIN opduser as b ON a.kskloginname = b.loginname
-WHERE date(a.ksklogintime) = CURRENT_DATE
-ORDER BY cdate,ctime DESC ";
-$queryr = pg_query($sqlr);
-
-$sql = " SELECT DISTINCT b.loginname,a.computername
-,date(a.ksklogintime) as cdate
-,to_char(a.ksklogintime, 'HH24:MI:SS') as ctime
-,a.department,a.ip_address,b.name
-FROM onlineuser as a
-INNER JOIN opduser as b ON a.kskloginname = b.loginname
-WHERE date(a.ksklogintime) = CURRENT_DATE
-ORDER BY cdate,ctime DESC
-LIMIT 1 ";
-$q = pg_query($sql);
-$r = pg_fetch_array($q);
-$new_nuer = $r['name'] . " | " . $r['loginname'] . " | " . $r['computername'] . " | " . $r['ip_address'] . " | " . $r['department'] . " | " . thaiDate($r['cdate']) . " | " . $r['ctime'];
-?>
+<?php include("config.inc.php");include('../config/my_con.class.php'); ?>
 <!doctype html>
-<html>
 
 <head>
-    <meta charset="utf-8">
-    <meta name="author" content="Yui Nakorn">
-    <title>รวมปัญหาHOSxp</title>
-    <link rel="stylesheet" type="text/css" href="css/DT_bst.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/bst.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Kanit" rel="stylesheet">
-    <script src="js/j182.js"></script>
-    <script src="js/j-dtb.js"></script>
-    <script src="js/DT_bst.js"></script>
+  <title>รวมปัญหาHOSxp</title>
+  <meta charset="utf-8">
+  <meta name="author" content="Yui Nakorn">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous" />
+  <link rel="stylesheet" type="text/css" href="css/bst.min.css" />
+  <script src="js/j182.js"></script>
+  <script src="js/j-dtb.js"></script>
+  <script src="js/DT_bst.js"></script>
 </head>
 
 <body>
-    <?php if (isset($_SESSION['username']) == "" || isset($_SESSION['username']) == null) {
-        echo "<script>window.location ='../login.php';</script>";
-    } ?>
-    <?php include 'menu.php'; ?>
+  <?php if (isset($_SESSION['username']) == "" || isset($_SESSION['username']) == null) {
+    echo "<script>window.location ='../login.php';</script>";
+  } ?>
+  <?php
+  $sqlr = " SELECT *  FROM cpareport_solving_problems ";
+  $queryr = mysqli_query($con,$sqlr);
+  ?>
 
-    <div class="container-ful">
-        <div class="row">
 
-        </div>
-        <div class="row">
-            <div class="container col-lg-10" style="margin-top: 10px">
-                <table cellpadding="0" cellspacing="0" border="0" class="table  table-bordered" id="example">
-                    <thead>
-                        <tr>
-                            <th>
-                                <CENTER> รายการ </CENTER>
-                            </th>
-                            <th>
-                                <CENTER> เพิ่มเติม </CENTER>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
 
-                        <?php  //  $ii = 1;
-                       // while ($rowr = pg_fetch_array($queryr)) {
-                            //   $i = str_pad($ii,4,"0",STR_PAD_LEFT);
-                            ?>
-                        <tr class="odd gradeX hoho">
-                            <!-- <td><CENTER><?= $i ?></CENTER></td> -->
-                            <td><?= $rowr[""] ?></td>
-                            <td><?= $rowr[""] ?></td>
-                        </tr>
-                        <?php $ii++;
-                       // } ?>
-                    </tbody>
-                </table>
-            </div>
-        </div>
 
+
+  <?php include 'menu.php'; ?>
+  <div class="container-fulid border" style="padding:20px">
+    <div class="row">
+      <div class="col-lg-3 col-12 border " style="margin-left:5px">
+        <form class="mt-3" action="#" method="POST">
+          <div class="form-group">
+            <h3>เพิ่มรายการ</h3>
+            <label for="namelist">ชื่อรายการ</label>
+            <input type="text" class="form-control" id="namelist" aria-describedby="emailHelp" placeholder="ชื่อรายการ" required />
+          </div>
+          <div class="form-group">
+            <label for="Info">รายละเอียด</label>
+            <textarea type="text" class="form-control" id="Info" placeholder="รายละเอียดปัญหา" required></textarea>
+          </div>
+          <div class="form-group">
+            <label for="namelist">link Google Doc </label>
+            <input type="text" class="form-control" id="namelist" aria-describedby="emailHelp" placeholder="เช่น https://docs.google.com/document/d/1p" required />
+          </div>
+          <button type="submit" class="btn btn-primary" name="Submit" value="ยืนยัน">Submit</button>
+        </form>
+      </div>
+
+
+      <div class="col">
+        <table cellpadding="0" cellspacing="0" border="0" class="table  table-bordered" id="example">
+          <thead>
+            <tr>
+            <th  >
+                <CENTER>ชื่อรายการ </CENTER>
+              </th>
+              <th  class="col">
+                <CENTER> รายละเอียดปัญหา </CENTER>
+              </th>
+              <th>
+                <CENTER> วิธีแก้ </CENTER>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <?php  while ($rowr = mysqli_fetch_assoc($queryr)) { ?>
+            <tr class="odd gradeX hoho">
+              <!-- <td><CENTER><?= $i ?></CENTER></td> -->
+              <td><?= $rowr["namelist"] ?></td>
+              <td><?= $rowr["list"] ?></td>
+              <td> <a href="<?echo  $rowr["link"]; ?>" target="blank">View</a></td>
+             
+            </tr>
+            <?php $ii++; } ?>
+          </tbody>
+        </table>
+      </div>
     </div>
+  </div>
+
+
 
 </body>
 
