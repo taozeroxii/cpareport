7bf43353d0361fbeb30ee87f25b2a71b7949e389
unknown <tao32040@gmail.com>
2019-08-21 11:41:47 +0700

    EDIT fix HOSXP add link google doc and color buttton

--- admin/fixHosxp.php
+++ admin/fixHosxp.php
@@ -1,10 +1,6 @@
 <?php session_start(); ?>
-<<<<<<< HEAD
-<?php include("config.inc.php");include('../config/my_con.class.php'); ?>
-=======
 <?php include("config.inc.php");
 include('../config/my_con.class.php'); ?>
->>>>>>> feb4ad4463a5df8458c44bf6452d626387e1b1d9
 <!doctype html>
 
 <head>
@@ -21,40 +17,6 @@ include('../config/my_con.class.php'); ?>
 <body>
   <?php if (isset($_SESSION['username']) == "" || isset($_SESSION['username']) == null) {
     echo "<script>window.location ='../login.php';</script>";
-<<<<<<< HEAD
-  } ?>
-  <?php
-  $sqlr = " SELECT *  FROM cpareport_solving_problems ";
-  $queryr = mysqli_query($con,$sqlr);
-  ?>
-
-
-
-
-
-  <?php include 'menu.php'; ?>
-  <div class="container-fulid border" style="padding:20px">
-    <div class="row">
-      <div class="col-lg-3 col-12 border " style="margin-left:5px">
-        <form class="mt-3" action="#" method="POST">
-          <div class="form-group">
-            <h3>เพิ่มรายการ</h3>
-            <label for="namelist">ชื่อรายการ</label>
-            <input type="text" class="form-control" id="namelist" aria-describedby="emailHelp" placeholder="ชื่อรายการ" required />
-          </div>
-          <div class="form-group">
-            <label for="Info">รายละเอียด</label>
-            <textarea type="text" class="form-control" id="Info" placeholder="รายละเอียดปัญหา" required></textarea>
-          </div>
-          <div class="form-group">
-            <label for="namelist">link Google Doc </label>
-            <input type="text" class="form-control" id="namelist" aria-describedby="emailHelp" placeholder="เช่น https://docs.google.com/document/d/1p" required />
-          </div>
-          <button type="submit" class="btn btn-primary" name="Submit" value="ยืนยัน">Submit</button>
-        </form>
-      </div>
-
-=======
   }
   $sqlr = " SELECT *  FROM cpareport_solving_problems ";
   $queryr = mysqli_query($con, $sqlr);
@@ -109,22 +71,11 @@ include('../config/my_con.class.php'); ?>
         </form>
       </div>
 
->>>>>>> feb4ad4463a5df8458c44bf6452d626387e1b1d9
 
       <div class="col">
         <table cellpadding="0" cellspacing="0" border="0" class="table  table-bordered" id="example">
           <thead>
             <tr>
-<<<<<<< HEAD
-            <th  >
-                <CENTER>ชื่อรายการ </CENTER>
-              </th>
-              <th  class="col">
-                <CENTER> รายละเอียดปัญหา </CENTER>
-              </th>
-              <th>
-                <CENTER> วิธีแก้ </CENTER>
-=======
               <th>
                 <CENTER>ชื่อรายการ </CENTER>
               </th>
@@ -136,22 +87,10 @@ include('../config/my_con.class.php'); ?>
               </th>
               <th>
                 <CENTER> เพิ่มเติม </CENTER>
->>>>>>> feb4ad4463a5df8458c44bf6452d626387e1b1d9
               </th>
             </tr>
           </thead>
           <tbody>
-<<<<<<< HEAD
-            <?php  while ($rowr = mysqli_fetch_assoc($queryr)) { ?>
-            <tr class="odd gradeX hoho">
-              <!-- <td><CENTER><?= $i ?></CENTER></td> -->
-              <td><?= $rowr["namelist"] ?></td>
-              <td><?= $rowr["list"] ?></td>
-              <td> <a href="<?echo  $rowr["link"]; ?>" target="blank">View</a></td>
-             
-            </tr>
-            <?php $ii++; } ?>
-=======
             <?php while ($rowr = mysqli_fetch_assoc($queryr)) { ?>
             <tr class="odd gradeX hoho">
               <td><?= $rowr["namelist"] ?></td>
@@ -168,7 +107,6 @@ include('../config/my_con.class.php'); ?>
             </tr>
             <?php $ii++;
             } ?>
->>>>>>> feb4ad4463a5df8458c44bf6452d626387e1b1d9
           </tbody>
         </table>
       </div>
