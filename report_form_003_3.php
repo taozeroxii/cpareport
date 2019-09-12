b103a3359b9ba0bdca3fb6f2a106493a25d1c9c1
unknown <tao32040@gmail.com>
2019-09-12 14:38:49 +0700

    Update report_form_003_3.php

--- report_form_003_3.php
+++ report_form_003_3.php
@@ -1,27 +1,27 @@
 <!DOCTYPE html>
 <html>
 <?php
-include"config/pg_con.class.php";
-include"config/func.class.php";
-include"config/time.class.php";
-include"config/head.class.php"; 
+include "config/pg_con.class.php";
+include "config/func.class.php";
+include "config/time.class.php";
+include "config/head.class.php";
 include('config/my_con.class.php');
-$bm = new Timer; 
+$bm = new Timer;
 $bm->start();
-for( $i = 0 ; $i < 100000 ; $i++ )
-{
+for ($i = 0; $i < 100000; $i++) {
 	$i;
 }
 
 $sql 		=  $_GET['sql'];
 $send_excel =  $_GET['sql'];
-$topLevelItems = " SELECT sql_code,sql_head FROM cpareport_sql WHERE sql_file = '".$sql."'";
-$res=mysqli_query($con,$topLevelItems);
-foreach($res as $item) {
+$topLevelItems = " SELECT sql_code,sql_head FROM cpareport_sql WHERE sql_file = '" . $sql . "'";
+$res = mysqli_query($con, $topLevelItems);
+foreach ($res as $item) {
 	$sql_detail = $item['sql_code'];
 	$sql_head   = $item['sql_head'];
 }
 ?>
+
 <body class="hold-transition skin-blue sidebar-mini">
 	<div class="wrapper">
 		<header class="main-header">
@@ -32,7 +32,7 @@ foreach($res as $item) {
 			<nav class="navbar navbar-static-top">
 				<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
 					<span class="sr-only">Toggle navigation</span>
-				</a> 
+				</a>
 			</nav>
 		</header>
 		<?php include "config/menuleft.class.php"; ?>
@@ -51,9 +51,9 @@ foreach($res as $item) {
 								<h3 class="box-title">
 									<div class="container">
 										<form class="form-inline" method="POST" action="#">
-											<input type="text" class="form-control" id="datepickers" placeholder="ช่วงวันที่เริ่ม" name="datepickers" data-provide="datepicker" data-date-language="th" autocomplete="off" >
-											<input type="text" class="form-control" id="datepickert" placeholder="ถึงวันที่" name="datepickert" data-provide="datepicker" data-date-language="th" autocomplete="off" >
-											  <select class="select2" name="pct_dropdown" id="pct_dropdown" style="width: 20%;" placeholder="คลินิก" title="เลือกคลินิก"></select>
+											<input type="text" class="form-control" id="datepickers" placeholder="ช่วงวันที่เริ่ม" name="datepickers" data-provide="datepicker" data-date-language="th" autocomplete="off">
+											<input type="text" class="form-control" id="datepickert" placeholder="ถึงวันที่" name="datepickert" data-provide="datepicker" data-date-language="th" autocomplete="off">
+											<select class="select2" name="pct_dropdown" id="pct_dropdown" style="width: 20%;" placeholder="คลินิก" title="เลือกคลินิก"></select>
 											<button type="submit" class="btn btn-default">Submit</button>
 										</form>
 									</div>
@@ -61,98 +61,106 @@ foreach($res as $item) {
 							</div>
 						</div>
 					</div>
-				</div>          
-				<?php 
+				</div>
+				<?php
 				$datepickers    = $_POST['datepickers'];
-				list($m,$d,$Y)  = split('/',$datepickers); 
-				$datepickers    = trim($Y)."-".trim($m)."-".trim($d);
+				list($m, $d, $Y)  = split('/', $datepickers);
+				$datepickers    = trim($Y) . "-" . trim($m) . "-" . trim($d);
 
 				$datepickert    = $_POST['datepickert'];
-				list($m,$d,$Y)  = split('/',$datepickert); 
-				$datepickert    = trim($Y)."-".trim($m)."-".trim($d);
+				list($m, $d, $Y)  = split('/', $datepickert);
+				$datepickert    = trim($Y) . "-" . trim($m) . "-" . trim($d);
 
-				$pct_dropdown   = $_POST['pct_dropdown'];    
+				$pct_dropdown   = $_POST['pct_dropdown'];
 
-				if($datepickers != "--") {
+				if ($datepickers != "--") {
 					$sql = " $sql_detail ";
 					$sql = str_replace("{datepickers}", "'$datepickers'", $sql);
 					$sql = str_replace("{datepickert}", "'$datepickert'", $sql);
 					$sql = str_replace("{pct_dropdown}", "'$pct_dropdown'", $sql);
 					$result = pg_query($sql);
+
+					$NAMEspclty = " SELECT * FROM spclty WHERE spclty = '" .$pct_dropdown. "'";
+					$resspclty = pg_query($conn,$NAMEspclty);
+					$resSpclty = pg_fetch_array($resspclty);
 					?>
 					<div class="row">
 						<div class="col-xs-12">
 							<div class="box">
 								<div class="box-header">
-									<h3 class="box-title co_dep"><?php echo " ข้อมูลช่วงวันที่ ".thaiDatefull($datepickers)." ถึงวันที่ ".thaiDatefull($datepickert) ?> 
-									<small><?php echo " เวลาที่ใช้ในการประมวลผล ".$bm->stop()." วินาที "; ?></small>
-								</h3>
-								<button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"> Template </button>
-								<!-- <button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" onclick="export_excel()" > Excel </button> -->
-							</div>
-							<div class="box-body table-responsive"><span class="fcol"> </span>
-								<table  class="table table-striped" >
-									<thead>
-										<tr>
-											<?php
-											$i = pg_num_fields($result);
-											for ($j = 0 ; $j < $i ; $j++) {
-												$fieldname = pg_field_name($result, $j);
-												echo '<th>' . $fieldname . '</th>';
-											}
-											?>
-										</tr> 
-									</thead>
-									<tbody>
-										<? $rw=0;
-										while($row_result = pg_fetch_array($result)) 
-										{ 
-											$rw++;
-											?>
+									<h3 class="box-title co_dep"><?php echo "แผนก: " .$resSpclty['name']  . "<br> ข้อมูลช่วงวันที่ " . thaiDatefull($datepickers) . " ถึงวันที่ " . thaiDatefull($datepickert) ?>
+										<small><?php echo " เวลาที่ใช้ในการประมวลผล " . $bm->stop() . " วินาที "; ?></small>
+									</h3>
+									<button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"> Template </button>
+									<!-- <button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" onclick="export_excel()" > Excel </button> -->
+								</div>
+								<div class="box-body table-responsive"><span class="fcol"> </span>
+									<table class="table table-striped">
+										<thead>
 											<tr>
 												<?php
-												for ($j = 0 ; $j < $i ; $j++) {
-													$fieldname = pg_field_name($result, $j);
-													echo '<td>' . $row_result[$fieldname] . '</td>';
-												} 
-												?>
+													$i = pg_num_fields($result);
+													for ($j = 0; $j < $i; $j++) {
+														$fieldname = pg_field_name($result, $j);
+														echo '<th>' . $fieldname . '</th>';
+													}
+													?>
 											</tr>
-											<?php  
-										}
-										?>                                   
-									</tbody>
-								</table>			
+										</thead>
+										<tbody>
+											<? $rw = 0;
+												while ($row_result = pg_fetch_array($result)) {
+													$rw++;
+													?>
+												<tr>
+													<?php
+															for ($j = 0; $j < $i; $j++) {
+																$fieldname = pg_field_name($result, $j);
+																echo '<td>' . $row_result[$fieldname] . '</td>';
+															}
+															?>
+												</tr>
+											<?php
+												}
+												?>
+										</tbody>
+									</table>
+								</div>
 							</div>
 						</div>
 					</div>
-				</div>  
-				<?php 
-			}
-			?>
-		</section>
-	</div>
-	<?php include"config/footer.class.php"; ?>
-	<?php include"config/js.class.php" ?>
-	<script>
-		$(function () {
-			$('#example1').DataTable()
-			$('#example2').DataTable({
-				'paging'      : true,
-				'lengthChange': false,
-				'searching'   : false,
-				'ordering'    : true,
-				'info'        : true,
-				'autoWidth'   : false
+				<?php
+				}
+				?>
+			</section>
+		</div>
+		<?php include "config/footer.class.php"; ?>
+		<?php include "config/js.class.php" ?>
+		<script>
+			$(function() {
+				$('#example1').DataTable()
+				$('#example2').DataTable({
+					'paging': true,
+					'lengthChange': false,
+					'searching': false,
+					'ordering': true,
+					'info': true,
+					'autoWidth': false
+				})
 			})
-		})
-	</script>
-	<script type="text/javascript">
-	//	function export_excel()
-	//	{
-	//		document.location = "export_excel_f002.php?c_department=<?php// echo $c_department; ?>&c_pttype=<?php //echo $c_pttype; ?>&send_excel=<?php //echo $send_excel; ?>//&datepickers=<?php //echo $datepickers; ?>&datepickert=<?php //echo $datepickert; ?>";
-	//	}
-	
-</script>
+		</script>
+		<script type="text/javascript">
+			//	function export_excel()
+			//	{
+			//		document.location = "export_excel_f002.php?c_department=<? php // echo $c_department; 
+																				?>&c_pttype=<?php //echo $c_pttype; 
+																							?>&send_excel=<?php //echo $send_excel; 
+																																		?>//&datepickers=<?php //echo $datepickers; 
+																																												?>&datepickert=<?php //echo $datepickert; 
+																																																						?>";
+			//	}
+		</script>
 
 </body>
+
 </html>
