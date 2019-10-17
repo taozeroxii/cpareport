19baa14381af4f7d9af4884287eb280d5199f069
unknown <tao32040@gmail.com>
2019-10-17 12:56:28 +0700

    Update er_index.php

--- er_index.php
+++ er_index.php
@@ -27,21 +27,6 @@ for( $i = 0 ; $i < 100000 ; $i++ )
 
 
 <body class="hold-transition skin-blue sidebar-mini">
-	<div class="wrapper">
-		<header class="main-header">
-			<a href="#" class="logo">
-				<span class="logo-mini"><b>r</b>CPA</span>
-				<span class="logo-lg"><b>Re</b>port Hospital</span>
-			</a>
-			<nav class="navbar navbar-static-top">
-				<a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-					<span class="sr-only">Toggle navigation</span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-			</nav>
-		</header>
 		<?php include "config/menuleft.class.php"; ?>
 		<div class="content-wrapper">
 			<section class="content-header">
