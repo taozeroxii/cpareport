dd3e952f46752964b97d6dd8986c30ea2dfc3315
unknown <tao32040@gmail.com>
2019-10-31 15:49:25 +0700

    Update timestampviewer.php
    แก้ไขเวลาเข้าหน้า report ให้จับการเชคค่าเป็นค่าปัจจุบันเช่นหากไม่ login ให้เก็บlog เป็ฯทั่วไป หาก login ห้เป็น usreนั้น ไม่เปลี่ยนชื่อผู้เข้าใช้งานเป็น loginล่าสุดทั้งหมด
    ***เดิมจะเก็บค่าล่าสุดจาก session นั้นๆโดยจะเปลี่ยนค่า user ทั้งหมดไปเป็นuser ล่าสุด**

--- config/timestampviewer.php
+++ config/timestampviewer.php
@@ -3,15 +3,16 @@
 $useronline = session_id();
 $userlogin = $_SESSION['username'];
 if(isset($_SESSION['username'])==''||isset($_SESSION['username'])==null){ $userlogin='ผู้เยี่ยมชมทั่วไป';}
+
 $time = time();
 $datenow = date("Y-m-d H:i:s");
 $sql2 = "SELECT * FROM viewer where session = '".$useronline."' AND sql_file = '$sql'";
 $result2 = mysqli_query($con,$sql2);
 $num = mysqli_num_rows($result2);
-if($num > 0){
-    $ud = ("UPDATE viewer set time_online = '" .$time. "',username = '".$userlogin."' where session = '".$useronline."'");
-    $uf = mysqli_query($con, $ud);
-    mysqli_query($con, $uf);
+if($num > 0 && $userlogin='ผู้เยี่ยมชมทั่วไป'){
+        $ud = ("UPDATE viewer set time_online = '" .$time. "',username = '".$userlogin."' where session = '".$useronline."'");
+        $uf = mysqli_query($con, $ud);
+        mysqli_query($con, $uf);
 }
 else{
     $insertlog = ("INSERT INTO viewer (session,username,sql_file,time_online,datetime) VALUES ('" . $useronline. "', '".$userlogin ."','" . $sql. " ',' " .$time. "' ,' " .$datenow. " ')");
