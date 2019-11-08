9f305513d6f3db5741349ea048b9c4013589c6db
unknown <tao32040@gmail.com>
2019-11-08 16:01:59 +0700

    Update index.php

--- kpi_thip/index.php
+++ kpi_thip/index.php
@@ -13,12 +13,16 @@
 date_default_timezone_set("Asia/Bangkok");
 $con = new mysqli("172.16.0.251", "report", "report", "cpareportdb");
 mysqli_set_charset($con, "utf8");
-$sql = " SELECT * FROM cpareport_kpi_thip ";
+$sql = "SELECT ct.kpi_code,kpi_name,kpi_cal_a,kpi_cal_b,kpi_ym,kpi_dateupdate FROM cpareport_kpi_thip ct  left join  cpareport_kpi_data cd on cd.kpi_code = ct.kpi_code and kpi_ym = (SELECT max(kpi_ym) FROM cpareport_kpi_data )";
 $result = mysqli_query($con, $sql);
 ?>
 <style>
-.col-lg-2{padding-left:100px;}
+  .col-lg-2 {
+    padding-left: 100px;
+  }
 </style>
+
+
 <body>
   <table class="table  table-bordered  table-hover">
     <div class="header" style=' background-color: #0B5345;font:blod;'>
@@ -37,23 +41,26 @@ $result = mysqli_query($con, $sql);
       foreach ($result as $item) {
         $kpiname  =  $item['kpi_name'];
         $kpicode  =  $item['kpi_code'];
+        $kpi_ym =  $item['kpi_ym'];
+        $a = $item['kpi_cal_a'];
+        $b = $item['kpi_cal_b'];
         $rw++;
         ?>
     <tbody>
       <tr class='tr-hovers'>
         <th style="color:green"><?= '&nbsp;&nbsp;&nbsp' . $rw ?></th>
-        <td style="text-align:center;"><?= $kpicode ?></td>
-        <td><?= $kpiname  ?></td>
-        <td style="text-align:center;">100.0</td>
-        <td>100.0</td>
-        <td>100.0</td>
+        <td style="text-align:center;"><?echo $kpicode ?></td>
+        <td><?= $kpiname; echo '<sub>  '.$kpi_ym.'</sub>'; ?> </td>
+        <td style="text-align:center;"><?if($a && $b!= null) echo number_format(($a/$b)*100,2) ;else echo 'NULL';?></td>
+        <td><?if($a != null) echo $a;else echo 'NULL';?></td>
+        <td><?if($b != null) echo $b;else echo 'NULL';?></td>
         <td data-toggle="modal" data-target="#<?= $kpicode ?>"><button class="btn btn-danger">!</button></td>
       </tr>
 
 
       <!-- Modal -->
-      <div class="modal fade" id="<?= $kpicode ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
-        <div class="modal-dialog" role="document">
+      <div class="modal fade modal-xl" id="<?= $kpicode ?>" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
           <div class="modal-content">
             <div class="modal-header">
               <h5 class="modal-title" id="exampleModalLongTitle"><?= $kpicode ?> </h5>
@@ -62,7 +69,16 @@ $result = mysqli_query($con, $sql);
               </button>
             </div>
             <div class="modal-body">
-              ...
+                <?
+                $sql = " SELECT * FROM cpareport_kpi_data WHERE kpi_code = '$kpicode' ORDER BY kpi_ym DESC  ";
+                $result1 = mysqli_query($con, $sql);
+                $resultMd = mysqli_fetch_assoc($result1);
+
+                echo 'ปี '.$resultMd['kpi_ym'];
+                echo '<br>a '.$resultMd['kpi_cal_a'];
+                echo '<br>b '.$resultMd['kpi_cal_b'];
+                ?>
+                
             </div>
             <div class="modal-footer">
               <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -80,6 +96,7 @@ $result = mysqli_query($con, $sql);
 
 
 
+
 </body>
 
-</html>
+</html> 
