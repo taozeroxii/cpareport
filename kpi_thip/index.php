52d53f6c19078464d03fb5b7d4b861087244f36a
eaktamp <eaktamp@gmail.com>
2019-11-08 10:41:52 +0700

    bar_kpi

--- kpi_thip/index.php
+++ kpi_thip/index.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/index.css">
   <link href="https://fonts.googleapis.com/css?family=Kanit&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
 </head>
 <?php 
 date_default_timezone_set("Asia/Bangkok");
@@ -18,26 +20,49 @@ $result = mysqli_query($con, $sql);
 <body>
 
   <div class="header">KPI THIP ABHAI BHUBAJHR HOSPITAL</div>
-<div class="main">
+
+<div class="container-fluid main">
+  <div class="row">
+    <div class="col-lg-8">DDDDDD</div>
+    <div class="col-lg-2">AAAAA</div>
+     <div class="col-lg-2">VVVV</div>
+
+      <?php
+          $rw == 0;
+          foreach ($result as $item) {
+          $kpiname  =  $item['kpi_name'];
+          $kpicode  =  $item['kpi_code'];
+          $rw++;
+          
+          ?>
+    <div class="col-lg-8"><?php echo $kpicode."  ".$kpiname;?></div>
+    <div class="col-lg-2">ssss</div>
+    <div class="col-lg-2">aaa</div>
+  <?php } ?>
+  </div>
+
+
+<!-- <div class="main">
   <div class="row">
     <div class="col-12 menu">
       <ul>
         <?php
-        $rw == 0;
+      /*  $rw == 0;
         foreach ($result as $item) {
           $kpiname  =  $item['kpi_name'];
           $kpicode  =  $item['kpi_code'];
           $rw++;
+          */
           ?>
 
-          <li><span class=""  data-toggle="modal" data-target="#<?php echo $kpicode;?>"><?php echo $kpicode."  ".$kpiname;?></span></li>
+          <li class=""  data-toggle="modal" data-target="#<?php// echo $kpicode;?>"><span><?php// echo $kpicode."  ".$kpiname;?></span></li>
 
-<div class="modal fade" id="<?php echo $kpicode;?>" role="dialog">
+<div class="modal fade" id="<?php// echo $kpicode;?>" role="dialog">
     <div class="modal-dialog modal-lg ">
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal">&times;</button>
-          <h4 class="modal-title"><span class="modal_kpi_code"><?php echo $kpicode;?></span> <sup><span class="modal_kpi_name"><?php echo $kpiname;?></span></sup></h4>
+          <h4 class="modal-title"><span class="modal_kpi_code"><?php// echo $kpicode;?></span> <sup><span class="modal_kpi_name"><?php// echo $kpiname;?></span></sup></h4>
         </div>
         <div class="modal-body">
           <p>Some text in the modal.</p>
@@ -50,7 +75,7 @@ $result = mysqli_query($con, $sql);
     </div>
   </div>
 
-        <?php } ?>
+        <?php //} ?>
       </ul>
     </div>
 
@@ -60,7 +85,7 @@ $result = mysqli_query($con, $sql);
       <p>test</p>
     </div>
   </div>
-  </div>
+  </div> -->
 
 
 
