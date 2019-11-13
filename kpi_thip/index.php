b45d234ef9259c5ff8fc2b5368bf9f5643be444b
unknown <tao32040@gmail.com>
2019-11-13 11:31:11 +0700

    Update index.php

--- kpi_thip/index.php
+++ kpi_thip/index.php
@@ -10,18 +10,20 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
   <script type="text/javascript" src="js/echarts.min.js"></script>
   <link href="https://fonts.googleapis.com/css?family=Prompt&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
 <?php
 date_default_timezone_set("Asia/Bangkok");
 $con = new mysqli("172.16.0.251", "report", "report", "cpareportdb");
 mysqli_set_charset($con, "utf8");
-$sql = "SELECT ct.kpi_code,kpi_name,kpi_cal_a,kpi_cal_b,kpi_ym,kpi_dateupdate FROM cpareport_kpi_thip ct  left join  cpareport_kpi_data cd on cd.kpi_code = ct.kpi_code and kpi_ym = (SELECT max(kpi_ym) FROM cpareport_kpi_data )";
+$sql = "SELECT ct.kpi_code,kpi_name,kpi_cal_a,kpi_cal_b,kpi_ym,kpi_dateupdate FROM cpareport_kpi_thip ct  left join  cpareport_kpi_data cd on cd.kpi_code = ct.kpi_code and kpi_ym = (SELECT max(kpi_ym) FROM cpareport_kpi_data )
+ group by ct.kpi_code,kpi_name,kpi_cal_a,kpi_cal_b,kpi_ym,kpi_dateupdate order by ct.id ";
 $result = mysqli_query($con, $sql);
 ?>
 <style>
   .col-lg-2 {
-    padding-left: 100px;
+    padding-left: 70px;
   }
 </style>
 
@@ -30,11 +32,12 @@ $result = mysqli_query($con, $sql);
   <table class="table  table-bordered  table-hover">
     <div class="header" style=' background-color: #0B5345;font:blod;'>
       <small>KPI THIP ABHAI BHUBAJHR HOSPITAL</small>
+       <small style="float:right"><button class="btn btn-warning btn-sm"><i class='fa fa-refresh'></i> Update</button></small>
       <hr>
       <div class="row">
         <div class="col-lg-1 col-md-2 col-1 " style="margin-top:-30px;">NO. &nbsp;&nbsp;&nbsp;&nbsp; KPICODE</div>
         <div class="col-lg-9 col-md-7 col-10" style="margin-top:-30px;">KPI-NAME.</div>
-        <div class="col-lg-2 col-md-3 col-1" style="margin-top:-30px;"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;ผล &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; B </div>
+        <div class="col-lg-2 col-md-3 col-1" style="margin-top:-30px;"> ผล &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; B </div>
       </div>
     </div>
     <div class="main"></div>
@@ -53,13 +56,13 @@ $result = mysqli_query($con, $sql);
       <tr class='tr-hovers'>
         <th style="color:green"><?= '&nbsp;&nbsp;&nbsp' . $rw ?></th>
         <td style="text-align:center;"><? echo $kpicode ?></td>
-        <td><?= $kpiname;
-              echo '<sub>  ' . $kpi_ym . '</sub>'; ?> </td>
+        <td><?= $kpiname; echo '<sub>  ' . $kpi_ym . '</sub>'; ?> </td>
         <td style="text-align:center;"><? if ($a && $b != null) echo number_format(($a / $b) * 100, 2);else echo 'NULL'; ?></td>
         <td><? if ($a != null) echo $a;
               else echo 'NULL'; ?></td>
         <td><? if ($b != null) echo $b;
               else echo 'NULL'; ?></td>
+        <td data-toggle="modal" data-target="#niyam<?= $kpicode ?>"><button class="btn btn-primary">?</button></td>
         <td data-toggle="modal" data-target="#<?= $kpicode ?>"><button class="btn btn-danger">!</button></td>
       </tr>
     <?php } ?>
@@ -84,7 +87,7 @@ $result = mysqli_query($con, $sql);
           <div class="modal-body">
           <center> <div id="report<?= $kpicode ?>" style="width:800px;height: 200px"></div></center><hr>
           <?php
-            $sqlchart = " SELECT * FROM cpareport_kpi_data WHERE kpi_code = '$kpicode' ORDER BY kpi_ym DESC  ";
+            $sqlchart = " SELECT * FROM cpareport_kpi_data WHERE kpi_code = '$kpicode' ORDER BY kpi_ym DESC  limit 10";
             $resultsqlchart = mysqli_query($con, $sqlchart);
             $title = [];
             $value = [];
@@ -170,9 +173,33 @@ $result = mysqli_query($con, $sql);
 
 
 
+  <!-- Modal -->
 
-
+  <!-- Modal -->
+  <? foreach ($result as $item) {
+    $kpiname  =  $item['kpi_name'];
+    $kpicode  =  $item['kpi_code']; ?>
+    <div class="modal fade modal-xl" id="niyam<?= $kpicode ?>" tabindex="-1" role="dialog" aria-labelledby="myExtraLargeModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLongTitle">
+              <h5><?= $kpicode . ' : ' . $kpiname; ?> </h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+          </div>
+          <div class="modal-body">
+          <center> <div id="report<?= $kpicode ?>" style="width:800px;height: 200px"></div></center><hr>
+        
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  <? } ?>
 
 </body>
-
 </html>
