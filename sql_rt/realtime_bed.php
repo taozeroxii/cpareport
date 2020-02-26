6fff6636628e113d2fd61bf9f8910586c886aea2
eaktamp <eaktamp@gmail.com>
2020-02-26 10:04:47 +0700

    update

--- sql_rt/realtime_bed.php
+++ sql_rt/realtime_bed.php
@@ -1,4 +1,5 @@
-SELECT count(an)as cc FROM ipt WHERE  dchdate IS NULL;<?php 
+<!-- SELECT count(an)as cc FROM ipt WHERE  dchdate IS NULL; -->
+<?php 
 include "pg_con.class.php";
 $sql_rt = " SELECT count(an)as cc FROM ipt WHERE  dchdate IS NULL; ";
 $result_rt = pg_query($sql_rt);
