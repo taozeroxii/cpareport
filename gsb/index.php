4a00e19ffc5057315f621301e3bc5ba56e08acb3
eaktamp <eaktamp@gmail.com>
2020-02-26 09:20:50 +0700

    GSB

---
+++ gsb/index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<?php
+//include 'connect.php';
+date_default_timezone_set('asia/bangkok');
+?>
+<script type="text/javascript" >
+  function date_time(id) {
+    date = new Date;
+    year = date.getFullYear();
+    month = date.getMonth();
+    months = new Array('มกราคม', 'กุมภาพันธ์', 'มีนาคม', 'เมษายน', 'พฤษภาคม', 'มิถุนายน', 'กรกฎาคม', 'สิงหาคม', 'กันยายน', 'ตุลาคม', 'พฤศจิกายน', 'ธันวาคม');
+    d = date.getDate();
+    day = date.getDay();
+    days = new Array('อาทิตย์', 'จันทร์', 'อังคาร', 'พุธ', 'พฤหัสดี', 'ศุกร์', 'เสาร์');
+    h = date.getHours();
+    if (h < 10) {
+      h = "0" + h;
+    }
+    m = date.getMinutes();
+    if (m < 10) {
+      m = "0" + m;
+    }
+    s = date.getSeconds();
+    if (s < 10) {
+      s = "0" + s;
+    }
+    result = 'วัน' + days[day] + ' ที่ ' + d + ' เดือน ' + months[month] + ' พ.ศ. ' + (year+543) + '  เวลา ' + h + ':' + m + ':' + s +' น.';
+    document.getElementById(id).innerHTML = result;
+    setTimeout('date_time("' + id + '");', '1000');
+    return true;
+  }
+</script>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title> | Seach Patient |</title>
+  <!-- <link href="https://fonts.googleapis.com/css?family=Shrikhand" rel="stylesheet"> -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="eak.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+  <div class="hmain">Patient GSB Member<sup> โรงพยาบาลเจ้าพระยาอภัยภูเบศร & ธนาคารออมสิน</sup></div>
+  <div class="container ">
+    <div class="row">
+      <div class="col-md-12">
+        <form class="form-inline" name="searchform" id="searchform">
+          <div class="form-group wi">
+            <label for="textsearch" ><!-- ttttt --></label>
+            <input type="text" name="itemname" id="itemname" class="form-control" placeholder=" ค้นหา เลขบัตรประชชาชน หรือ ชื่อ หรือ นามสกุล || อย่างใดอย่างหนึ่ง " autocomplete="off"  autofocus/>
+          </div>
+          <button type="button" class="btn success" id="btnSearch" title="">
+            <span class="glyphicon glyphicon-search" ></span>
+            ค้นหาข้อมูลผู้มีสิทธิ์
+          </button>
+          &nbsp;
+          <button type="button" class="btn btn-light dtim" id="">
+            <span id="date_time" class="dtim">
+            </span> 
+            <script type="text/javascript"> window.onload = date_time('date_time');</script> 
+          </button>
+
+
+        </form>
+      </div>
+    </div>
+
+    <center><div class="loading">&nbsp;</div></center>
+
+
+    <div class="row" id="list-data" style="margin-top: 10px;"></div>
+
+    <button id="scrollToTopButton" onclick="scrollToTop(100,3);">
+      <i class="fa fa-arrow-up"></i></button>
+    </div>
+    
+    <script type="text/javascript" src="jquery-1.11.2.min.js"></script>
+    <script type="text/javascript">
+      $(function () {
+        $("#btnSearch").click(function () {
+          $.ajax({
+            url: "search.php",
+            type: "post",
+            data: {itemname: $("#itemname").val()},
+            beforeSend: function () {
+              $(".loading").show();
+            },
+            complete: function () {
+              $(".loading").hide();
+            },
+            success: function (data) {
+              $("#list-data").html(data);
+            }
+          });
+        });
+        $("#searchform").on("keyup keypress",function(e){
+         var code = e.keycode || e.which;
+         if(code==13){
+           $("#btnSearch").click();
+           return false;
+         }
+       });
+      });
+// ############### scoll top
+var html, body, scrollToTopButton;
+window.onload = function() {
+  html = document.documentElement;
+  body = document.body;
+  scrollToTopButton = document.getElementById("scrollToTopButton");
+};
+function scrollToTop(totalTime, easingPower) {
+  var timeInterval = 1;
+  var scrollTop = Math.round(body.scrollTop || html.scrollTop);
+  var timeLeft = totalTime;
+  var scrollByPixel = setInterval(function() {
+    var percentSpent = (totalTime - timeLeft) / totalTime;
+    if (timeLeft >= 0) {
+      var newScrollTop = scrollTop * (1 - easeInOut(percentSpent, easingPower));
+      body.scrollTop = newScrollTop;
+      html.scrollTop = newScrollTop;
+      timeLeft--;
+    } else {
+      clearInterval(scrollByPixel);
+    }
+  }, timeInterval);
+}
+function easeInOut(t, power) {
+  if (t < 1.5) {
+    return 0.5 * Math.pow(2 * t, power);
+  } else {
+    return 0.5 * (2 - Math.pow(2 * (1 - t), power));
+  }
+}
+window.onscroll = controlScrollToTopButton;
+function controlScrollToTopButton() {
+  var windowInnerHeight = 2 * window.innerHeight;
+  if (
+    body.scrollTop > windowInnerHeight ||
+    html.scrollTop > windowInnerHeight
+    ) {
+    scrollToTopButton.classList.add("show");
+} else {
+  scrollToTopButton.classList.remove("show");
+}
+}
+</script>
+</body>
+</html>
---
+++ gsb/search.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<?php
+include 'connect.php';
+date_default_timezone_set('asia/bangkok');
+function thaiDate($datetime)
+{
+   if(!is_null($datetime))
+   {
+     list($date,$time) = split('T',$datetime);
+     list($Y,$m,$d) = split('-',$date);
+     $Y = $Y;
+     switch($m)
+     {
+        case "01":$m = "ม.ค."; break;
+        case "02":$m = "ก.พ."; break;
+        case "03":$m = "มี.ค."; break;
+        case "04":$m = "เม.ย."; break;
+        case "05":$m = "พ.ค."; break;
+        case "06":$m = "มิ.ย."; break;
+        case "07":$m = "ก.ค."; break;
+        case "08":$m = "ส.ค."; break;
+        case "09":$m = "ก.ย."; break;
+        case "10":$m = "ต.ค."; break;
+        case "11":$m = "พ.ย."; break;
+        case "12":$m = "ธ.ค."; break;
+    }
+    return $d." ".$m." ".$Y."";
+}
+return "";
+}
+
+$sql = "   SELECT *
+           FROM gsb_datacheck
+           WHERE 1 = 1 
+           AND fname   like '%{$_POST['itemname']}%'
+           OR  lname   like '%{$_POST['itemname']}%'
+           OR  gsb_cid like '%{$_POST['itemname']}%' ";
+                    if($_POST['itemname']!= NULL) {
+                        $query = mysql_query($sql);
+                        ?>
+                        <body>
+                            <div class="col-md-12">
+                                <table class="table table-bordered ta">
+                                    <thead>
+                                        <tr class="detail">
+                                            <th style="text-align: center;">#</th>
+                                            <th style="text-align: center;">PersonID</th>
+                                            <th style="text-align: center;">ชื่อ-สกุล</th>
+                                            <th style="text-align: center;">อายุ (ปี)</th>
+                                            <th style="text-align: center;">เลขที่บัตรประชาชน</th>
+                                             <th style="text-align: center;" >วันที่เริ่ม</th>
+                                              <th style="text-align: center;" >วันที่สิ้นสุด</th>
+                                            <th style="text-align: center;" >ประเภท</th>
+                                            <th style="text-align: center;" >สถานที่</th>
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <?php $i=1; while ($result = mysql_fetch_assoc($query)) { ?>
+                                        <tr class="list" >
+                                            <td><?php echo $i;?></td>
+                                            <td><?php echo $result['gsb_personid'];?></td>
+                                            <td><?php echo $result['pname']."".$result['fname']."&nbsp;&nbsp;".$result['lname'];?></td>
+                                            <td style="text-align: center;"><?php echo $result['gsb_age'];?></td>
+                                            <td><?php echo $result['gsb_cid'];?></td>
+                                            <td><?php echo thaiDate($result['gsb_startdate']);?></td>
+                                            <td><?php echo thaiDate($result['gsb_enddate']);?></td>
+                                            <td><?php echo $result['gsb_emp_type'];?></td>
+                                            <td><?php echo $result['gsb_groupname'];?></td>
+
+                                        </tr>
+                                        <?php $i++; } ?>
+                                    </tbody>
+                                </table>
+                                <?php    }
+                                else
+                                {
+                                    echo "<script type='text/javascript'>alert('คุณไม่ได้ระบุข้อมูลที่ต้องการค้นหา กรุณาระบุข้อมูลอย่างใดอย่างหนึ่ง เพื่อค้นหา!')</script>";
+                                }
+                                ?>
+                            </div>
+                        </body>
+                        </html>
