9d206558a32710fc984046ed73beab43472e0ab5
eaktamp <eaktamp@gmail.com>
2020-03-26 10:58:42 +0700

    covid api

---
+++ covid.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<iframe width="100%" height="1500" src="https://data.go.th/dataset/covid-19-daily?id=93f74e67-6f76-4b25-8f5d-b485083100b6&is_fullscreen=1" frameBorder="0"></iframe>
+</body>
+</html>
