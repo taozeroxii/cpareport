3de3d1f3089b9099b82f76e87babfbbf8c63f900
unknown <tao32040@gmail.com>
2020-03-27 09:46:46 +0700

    เพิ่ม login helphos

--- help/index.php
+++ help/index.php
@@ -1,10 +1,18 @@
+<?php
+session_start(); 
+include('../config/my_con.class.php');
+$s = $_GET['search_menu'];
+$topLevelItems = " SELECT * FROM cpareport_menu WHERE menu_status in ('1','2') and menu_sub LIKE '%%$s%%'  ";
+$res = mysqli_query($con, $topLevelItems);
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<title>LINE ADMIN</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+	<link rel="icon" type="image/png" href="images/icons/favicon.ico" />
 	<link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
@@ -14,14 +22,18 @@
 	<link rel="stylesheet" type="text/css" href="css/util.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 </head>
+
 <body>
+	<?php if (isset($_SESSION['username']) == "" || isset($_SESSION['username']) == null) {
+		echo "<script>window.location ='../login.php';</script>";
+	} ?>
 	<div class="container-contact100" style="background-image: url('images/bg-01.jpg');">
 		<div class="wrap-contact100">
 			<form class="contact100-form validate-form" name="send" id="send" action="send.php" method="post">
 				<span class="contact100-form-title">
 					Admin Help HosXp
 				</span>
-<!-- 
+				<!-- 
 				<div class="wrap-input100 rs1-wrap-input100 validate-input" data-validate="Name is required">
 					<span class="label-input100">Tell us your name *</span>
 					<input class="input100" type="text" name="name" placeholder="Enter your name">
@@ -36,7 +48,7 @@
 					<span class="label-input100">Your Website</span>
 					<input class="input100" type="text" name="web" placeholder="http://">
 				</div> -->
-				<div class="wrap-input100 validate-input" data-validate = "Message is required">
+				<div class="wrap-input100 validate-input" data-validate="Message is required">
 					<span class="label-input100">ข้อความ</span>
 					<textarea class="input100" name="message" id="message" placeholder=""></textarea>
 				</div>
@@ -63,10 +75,14 @@
 	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
 	<script>
 		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
 		gtag('js', new Date());
 
 		gtag('config', 'UA-23581568-13');
 	</script>
 </body>
+
 </html>
