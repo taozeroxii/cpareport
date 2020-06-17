385790d1ac9f3122940db0c1eef7f556a7855981
eaktamp <eaktamp@hotmail.com>
2020-06-17 12:31:37 +0700

    css status

--- network_ad_user_check/search.php
+++ network_ad_user_check/search.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title></title>
 </head>
@@ -8,103 +9,161 @@ include 'connect.php';
 date_default_timezone_set('asia/bangkok');
 function thaiDate($datetime)
 {
-   if(!is_null($datetime))
-   {
-     list($date,$time) = split('T',$datetime);
-     list($Y,$m,$d) = split('-',$date);
-     $Y = $Y;
-     switch($m)
-     {
-        case "01":$m = "ม.ค."; break;
-        case "02":$m = "ก.พ."; break;
-        case "03":$m = "มี.ค."; break;
-        case "04":$m = "เม.ย."; break;
-        case "05":$m = "พ.ค."; break;
-        case "06":$m = "มิ.ย."; break;
-        case "07":$m = "ก.ค."; break;
-        case "08":$m = "ส.ค."; break;
-        case "09":$m = "ก.ย."; break;
-        case "10":$m = "ต.ค."; break;
-        case "11":$m = "พ.ย."; break;
-        case "12":$m = "ธ.ค."; break;
+    if (!is_null($datetime)) {
+        list($date, $time) = split('T', $datetime);
+        list($Y, $m, $d) = split('-', $date);
+        $Y = $Y;
+        switch ($m) {
+            case "01":
+                $m = "ม.ค.";
+                break;
+            case "02":
+                $m = "ก.พ.";
+                break;
+            case "03":
+                $m = "มี.ค.";
+                break;
+            case "04":
+                $m = "เม.ย.";
+                break;
+            case "05":
+                $m = "พ.ค.";
+                break;
+            case "06":
+                $m = "มิ.ย.";
+                break;
+            case "07":
+                $m = "ก.ค.";
+                break;
+            case "08":
+                $m = "ส.ค.";
+                break;
+            case "09":
+                $m = "ก.ย.";
+                break;
+            case "10":
+                $m = "ต.ค.";
+                break;
+            case "11":
+                $m = "พ.ย.";
+                break;
+            case "12":
+                $m = "ธ.ค.";
+                break;
+        }
+        return $d . " " . $m . " " . $Y . "";
     }
-    return $d." ".$m." ".$Y."";
-}
-return "";
+    return "";
 }
 
 function thdate($datetime)
 {
-   if(!is_null($datetime))
-   {
-     list($date,$time) = split('T',$datetime);
-     list($Y,$m,$d) = split('-',$date);
-     $Y = $Y+543;
-     switch($m)
-     {
-        case "01":$m = "ม.ค."; break;
-        case "02":$m = "ก.พ."; break;
-        case "03":$m = "มี.ค."; break;
-        case "04":$m = "เม.ย."; break;
-        case "05":$m = "พ.ค."; break;
-        case "06":$m = "มิ.ย."; break;
-        case "07":$m = "ก.ค."; break;
-        case "08":$m = "ส.ค."; break;
-        case "09":$m = "ก.ย."; break;
-        case "10":$m = "ต.ค."; break;
-        case "11":$m = "พ.ย."; break;
-        case "12":$m = "ธ.ค."; break;
+    if (!is_null($datetime)) {
+        list($date, $time) = split('T', $datetime);
+        list($Y, $m, $d) = split('-', $date);
+        $Y = $Y + 543;
+        switch ($m) {
+            case "01":
+                $m = "ม.ค.";
+                break;
+            case "02":
+                $m = "ก.พ.";
+                break;
+            case "03":
+                $m = "มี.ค.";
+                break;
+            case "04":
+                $m = "เม.ย.";
+                break;
+            case "05":
+                $m = "พ.ค.";
+                break;
+            case "06":
+                $m = "มิ.ย.";
+                break;
+            case "07":
+                $m = "ก.ค.";
+                break;
+            case "08":
+                $m = "ส.ค.";
+                break;
+            case "09":
+                $m = "ก.ย.";
+                break;
+            case "10":
+                $m = "ต.ค.";
+                break;
+            case "11":
+                $m = "พ.ย.";
+                break;
+            case "12":
+                $m = "ธ.ค.";
+                break;
+        }
+        return $d . " " . $m . " " . $Y . "";
     }
-    return $d." ".$m." ".$Y."";
-}
-return "";
+    return "";
 }
 
 $sql = "   SELECT *,firstname,lastname,username,department,jobtitle
            FROM network_ad_user 
            WHERE 1 = 1 
-           AND flage = '1'
+           AND flage <> '2'
            AND firstname  like '%{$_POST['itemname']}%'
            OR  lastname   like '%{$_POST['itemname']}%' ";
-                    if($_POST['itemname'] != NULL) {
-                        $query = mysql_query($sql);
-                        ?>
-                        <body>
-                            <div class="col-md-12">
-                                <table class="table table-bordered ta">
-                                    <thead>
-                                        <tr class="detail">
-                                            <th style="text-align: center;">#</th>
-                                            <th style="text-align: center;">ชื่อ</th>
-                                            <th style="text-align: center;">สกุล</th>
-                                            <th style="text-align: center;">User</th>
-                                            <th style="text-align: center;" >กลุ่มงาน</th>
-                                            <th style="text-align: center;" >ตำแหน่ง</th>
-       
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php $i=1; while ($result = mysql_fetch_assoc($query)) { ?>
-                                        <tr class="list" >
-                                            <td><?php echo $i;?>.</td>
-                                            <td><?php echo $result['firstname'];?></td>
-                                            <td><?php echo $result['lastname'];?></td>
-                                            <td><?php echo $result['username'];?></td>
-                                            <td><?php echo $result['department'];?></td>
-                                            <td><?php echo $result['jobtitle'];?></td>
+if ($_POST['itemname'] != NULL) {
+    $query = mysql_query($sql);
+?>
 
-                                        </tr>
-                                        <?php $i++; } ?>
-                                    </tbody>
-                                </table>
-                                <?php   
-                                 }
-                                else
-                                {
-                                    echo "<script type='text/javascript'>alert('คุณไม่ได้ระบุชื่อ หรือ นามสกุล อย่างใดอย่างหนึ่ง เพื่อค้นหา!')</script>";
+    <body>
+        <div class="col-md-12">
+            <table class="table table-bordered ta">
+                <thead>
+                    <tr class="detail">
+                        <th style="text-align: center;">#</th>
+                        <th style="text-align: center;">ชื่อ</th>
+                        <th style="text-align: center;">สกุล</th>
+                        <th style="text-align: center;">User</th>
+                        <th style="text-align: center;">กลุ่มงาน</th>
+                        <th style="text-align: center;">ตำแหน่ง</th>
+                        <th style="text-align: center;">สถานะ</th>
+
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1;
+                    while ($result = mysql_fetch_assoc($query)) { ?>
+                        <tr class="list">
+                            <td><?php echo $i; ?>.</td>
+                            <td><?php echo $result['firstname']; ?></td>
+                            <td><?php echo $result['lastname']; ?></td>
+                            <td><?php echo $result['username']; ?></td>
+                            <td><?php echo $result['department']; ?></td>
+                            <td><?php echo $result['jobtitle']; ?></td>
+                            <td>
+                                <?php $cs  = $result['flage'];
+                                if ($cs == "1") {
+                                    $cs =  "<span class='cs1'>เปิดใช้งาน</span>";
+                                } elseif ($cs == "0") {
+                                    $cs =  "<span class='cs2'>รอยืนยันการใช้งาน</span>";
+                                } elseif ($cs == "3") {
+                                    $cs =  "<span class='cs3'>ปิดใช้งาน</span>";
                                 }
+                                echo $cs;
                                 ?>
-                            </div>
+                            </td>
+                        </tr>
+                    <?php $i++;
+                    } ?>
+                </tbody>
+            </table>
+        <?php
+    } else {
+        echo "<script type='text/javascript'>alert('คุณไม่ได้ระบุชื่อ หรือ นามสกุล อย่างใดอย่างหนึ่ง เพื่อค้นหา!')</script>";
+    }
+        ?>
+        </div>
 
-                        </body>
-                        </html>
+    </body>
+
+</html>
