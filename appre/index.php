d3ad8b0055f4ee5139a018eacd982d837f9698bb
eaktamp <eaktamp@hotmail.com>
2020-08-14 10:44:49 +0700

    appre

---
+++ appre/index.php
@@ -0,0 +1,194 @@
+<?php
+include "../config/pg_con.class.php";
+include "../config/func.class.php";
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title></title>
+	<script src="images/jquery.js"></script>
+	<link rel="stylesheet" type="text/css" href="images/standalo.css" />
+	<link rel="stylesheet" type="text/css" href="images/tabs-acc.css" />
+	<meta http-equiv="Content-Type" content="text/html;charset=windows-1252">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<link href="https://fonts.googleapis.com/css2?family=Kanit&display=swap" rel="stylesheet">
+
+</head>
+
+<body>
+
+<form class="w3-container" action="#" method="GET">
+  <h2 class="mmm">รายการนัดตามคลินิก  | Refill | ยาไปรษณีย์ | เติมยา</h2>
+  <input class="w3-input " name="sdate" id="sdate" type="date" required="required" type="" style="width:20%" placeholder="" title="เลือกระบุวันที่เริ่มต้น">
+  <input class="w3-input " name="edate" id="edate"type="date" required="required" type="" style="width:20%" placeholder="" title="เลือกระบุวันที่สิ้นสุด"><br>
+  <button class="button button2" type="submit" value="">ค้นหาข้อมูล</button>
+</form>
+<hr>
+<?php 
+$sdate	= $_GET['sdate'];
+$edate	= $_GET['edate'];
+IF(isset($sdate)) { ?>
+
+<div class="mainh"> ข้อมูลช่วงวันที่&nbsp;&nbsp;<span class="ddd"> <?php echo thaidate($sdate)." -  ".thaidate($edate); ?><span> </div>
+
+	<div id="accordion">
+		<?php
+
+		$sql = " SELECT c.name AS clinic ,c.location 
+				FROM oapp as o
+				INNER JOIN clinic as c ON c.clinic = o.clinic
+				INNER JOIN oapp_cause as p on p.name = o.app_cause
+				WHERE 1 = 1
+				AND active_status = 'Y'
+				AND o.nextdate BETWEEN '$sdate' AND  '$edate'
+				AND app_cause IN ('รับยา Refill ครั้งที่ 1','รับยา Refill ครั้งที่ 2','รับยาไปรษณีย์ครั้งที่ 1','รับยาไปรษณีย์ครั้งที่ 2','เติมยาครั้งที่ 1','เติมยาครั้งที่ 2','เติมยาครั้งที่ 3')
+				GROUP BY c.name,c.location
+				ORDER BY c.location ASC , c.name ASC ";
+		$result = pg_query($sql);
+		$rw = 0;
+		while ($row_result = pg_fetch_array($result)) {
+			$rw++;
+			$clinic	 = $row_result['clinic'];
+
+		?>
+
+			<h2 class="hhh" title="คลิกดูรายการ"><?php echo $clinic; ?></h2>
+
+			<div class="pane">
+				<!-- <h3><?php// echo $clinic; ?></h3> -->
+				<!-- <hr> -->
+				<table>
+					<tr >
+						<th class="trh">วันที่</th>
+						<th class="trh">รับยา Refill ครั้งที่ 1</th>
+						<th class="trh">รับยา Refill ครั้งที่ 2</th>
+						<th class="trh">รับยาไปรษณีย์ครั้งที่ 1</th>
+						<th class="trh">รับยาไปรษณีย์ครั้งที่ 2</th>
+						<th class="trh">เติมยาครั้งที่ 1</th>
+						<th class="trh">เติมยาครั้งที่ 2</th>
+						<th class="trh">เติมยาครั้งที่ 3</th>
+						<th class="trh">รวม</th>
+					</tr>
+					<?php
+					$sql2 = " SELECT o.nextdate
+					,c.name AS clinic
+					-- ,count(*) as total
+	  ,SUM ( CASE WHEN  p.display_order = '79' THEN 1 ELSE 0 END ) AS  r1
+	  ,SUM ( CASE WHEN  p.display_order = '80' THEN 1 ELSE 0 END ) AS  r2
+	  ,SUM ( CASE WHEN  p.display_order = '74' THEN 1 ELSE 0 END ) AS  r3
+	  ,SUM ( CASE WHEN  p.display_order = '75' THEN 1 ELSE 0 END ) AS  r4
+	  ,SUM ( CASE WHEN  p.display_order = '81' THEN 1 ELSE 0 END ) AS  r5
+	  ,SUM ( CASE WHEN  p.display_order = '82' THEN 1 ELSE 0 END ) AS  r6
+	  ,SUM ( CASE WHEN  p.display_order = '83' THEN 1 ELSE 0 END ) AS  r7
+	  ,SUM ( CASE WHEN  p.display_order in ('79','80','74','75','81','82','83') THEN 1 ELSE 0 END ) AS  r8
+	  
+FROM oapp as o
+INNER JOIN clinic as c ON c.clinic = o.clinic
+INNER JOIN oapp_cause as p on p.name = o.app_cause
+WHERE 1 = 1
+AND o.nextdate BETWEEN '$sdate' AND  '$edate'
+AND c.name = '$clinic'
+AND app_cause IN ('รับยา Refill ครั้งที่ 1','รับยา Refill ครั้งที่ 2','รับยาไปรษณีย์ครั้งที่ 1','รับยาไปรษณีย์ครั้งที่ 2','เติมยาครั้งที่ 1','เติมยาครั้งที่ 2','เติมยาครั้งที่ 3')
+GROUP BY o.nextdate,c.name
+ORDER BY o.nextdate DESC ";
+					$result2 = pg_query($sql2);
+
+					while ($row_result2 = pg_fetch_array($result2)) {
+					?>
+						<tr>
+							<td class="trdate"><?php echo thaidate($row_result2['nextdate']); ?></td>
+							<td class="trd">
+								<?php  $r1 = $row_result2['r1']; 
+									 if($r1 == "0"){
+										 $r1 = "<span class='fff'>-</span>";
+									 }
+									 	$r1 = "<span class='ttt'>".$r1."</span>";
+									 echo $r1;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r2 = $row_result2['r2']; 
+									 if($r2 == "0"){
+										 $r2 = "<span class='fff'>-</span>";
+									 }
+									 	$r2 = "<span class='ttt'>".$r2."</span>";
+									 echo $r2;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r3 = $row_result2['r3']; 
+									 if($r3 == "0"){
+										 $r3 = "<span class='fff'>-</span>";
+									 }
+									 	$r3 = "<span class='ttt'>".$r3."</span>";
+									 echo $r3;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r4 = $row_result2['r4']; 
+									 if($r4 == "0"){
+										 $r4 = "<span class='fff'>-</span>";
+									 }
+									 	$r4 = "<span class='ttt'>".$r4."</span>";
+									 echo $r4;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r5 = $row_result2['r5']; 
+									 if($r5 == "0"){
+										 $r5 = "<span class='fff'>-</span>";
+									 }
+									 	$r5 = "<span class='ttt'>".$r5."</span>";
+									 echo $r5;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r6 = $row_result2['r6']; 
+									 if($r6 == "0"){
+										 $r6 = "<span class='fff'>-</span>";
+									 }
+									 	$r6 = "<span class='ttt'>".$r6."</span>";
+									 echo $r6;
+								?>
+							</td>
+							<td class="trd">
+								<?php  $r7 = $row_result2['r7']; 
+									 if($r7 == "0"){
+										 $r7 = "<span class='fff'>-</span>";
+									 }
+									 	$r7 = "<span class='ttt'>".$r7."</span>";
+									 echo $r7;
+								?>
+							</td>
+							
+							<td class="trtotal"><?php echo $row_result2['r8']; ?></td>
+						</tr>
+					<?php
+					}
+					?>
+				</table>
+<br>
+			</div>
+
+		<?php
+		}
+		?>
+	</div>
+	<?php } ?>
+
+
+	
+	<script>
+		$(function() {
+			$("#accordion").tabs("#accordion div.pane", {
+				tabs: 'h2',
+				effect: 'slide',
+				initialIndex: null
+			});
+		});
+	</script>
+</body>
+
+</html>
---
+++ oapp_clinic.php
@@ -0,0 +1,257 @@
+<!DOCTYPE html>
+<html>
+<?php
+include"config/pg_con.class.php";
+include"config/func.class.php";
+include"config/time.class.php";
+include"config/head.class.php"; 
+include('config/my_con.class.php');
+session_start();
+$bm = new Timer; 
+$bm->start();
+for( $i = 0 ; $i < 100000 ; $i++ )
+{
+	$i;
+}
+/*
+$sql 		=  $_GET['sql'];
+$send_excel =  $_GET['sql'];
+$topLevelItems = " SELECT sql_code,sql_head FROM cpareport_sql WHERE sql_file = '".$sql."'";
+$res=mysqli_query($con,$topLevelItems);
+foreach($res as $item) {
+	$sql_detail = $item['sql_code'];
+	$sql_head   = $item['sql_head'];
+}
+*/
+include "config/timestampviewer.php";//เรียกไฟล์ในส่วนที่ทำงานนับจำนวนผู้กดเข้ามาหน้า sql นั้นๆ
+?>
+<body class="hold-transition skin-blue sidebar-mini">
+		<?php include "config/menuleft.class.php"; ?>
+		<div class="content-wrapper">
+			<section class="content-header">
+				<h1>
+					<?php echo $sql_head; ?>
+					<small><?php echo 'Viewer: '.$countview; ?></small>
+				</h1>
+			</section>
+			<section class="content">
+
+				<div class="row">
+					<div class="col-xs-12">
+						<div class="box">
+							<div class="box-header">
+								<h3 class="box-title">
+									<div class="container">
+										<form class="form-inline" method="POST" action="#">
+											<input type="text" class="form-control" id="datepickers" placeholder="ช่วงวันที่เริ่ม" name="datepickers" data-provide="datepicker" data-date-language="th" autocomplete="off" >
+											<input type="text" class="form-control" id="datepickert" placeholder="ถึงวันที่" name="datepickert" data-provide="datepicker" data-date-language="th" autocomplete="off" >
+											<!-- <select class="select2" name="cli_dropdown" id="cli_dropdown" style="width: 20%;" placeholder="คลินิก" title="เลือกคลินิก"></select> -->
+											<button type="submit" class="btn btn-default">ค้นหาข้อมูล</button>
+										</form>
+									</div>
+								</h3>
+							</div>
+						</div>
+					</div>
+				</div>          
+				<?php 
+				$datepickers    = $_POST['datepickers'];
+				list($m,$d,$Y)  = split('/',$datepickers); 
+				$datepickers    = trim($Y)."-".trim($m)."-".trim($d);
+
+				$datepickert    = $_POST['datepickert'];
+				list($m,$d,$Y)  = split('/',$datepickert); 
+				$datepickert    = trim($Y)."-".trim($m)."-".trim($d);
+
+			//	$cli_dropdown   = $_POST['cli_dropdown'];    
+
+				if($datepickers != "--") {
+					$sql = " SELECT o.vstdate :: DATE as vdate,
+                    SUM ( CASE WHEN  ap.clinic IS NOT NULL  THEN 1 ELSE 0 END ) AS  รวม,
+                    SUM ( CASE WHEN  ap.clinic = '036' THEN 1 ELSE 0 END ) AS  คลินิกนรีเวชกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '061' THEN 1 ELSE 0 END ) AS  คลินิกอายุรกรรมโรคติดเชื้อ,
+                    SUM ( CASE WHEN  ap.clinic = '022' THEN 1 ELSE 0 END ) AS  คลินิกโรคข้ออายุรกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '066' THEN 1 ELSE 0 END ) AS  คลินิกโรคพาร์กินสัน,
+                    SUM ( CASE WHEN  ap.clinic = '050' THEN 1 ELSE 0 END ) AS  คลินิกโรคพาร์กินสัน TTM,
+                    SUM ( CASE WHEN  ap.clinic = '023' THEN 1 ELSE 0 END ) AS  คลินิกโรคทางเดินอาหาร,
+                    SUM ( CASE WHEN  ap.clinic = '001' THEN 1 ELSE 0 END ) AS  คลินิกเบาหวาน,
+                    SUM ( CASE WHEN  ap.clinic = '003' THEN 1 ELSE 0 END ) AS  คลินิกมะเร็ง,
+                    SUM ( CASE WHEN  ap.clinic = '025' THEN 1 ELSE 0 END ) AS  คลินิกโรคเลือด,
+                    SUM ( CASE WHEN  ap.clinic = '084' THEN 1 ELSE 0 END ) AS  คลินิกโรคเลือด ธาลัสซีเมีย,
+                    SUM ( CASE WHEN  ap.clinic = '020' THEN 1 ELSE 0 END ) AS  คลินิกโรคหัวใจ,
+                    SUM ( CASE WHEN  ap.clinic = '052' THEN 1 ELSE 0 END ) AS  คลินิกเลิกบุหรี่,
+                    SUM ( CASE WHEN  ap.clinic = '094' THEN 1 ELSE 0 END ) AS  ห้องตรวจคลินิกระบบทางเดินหายใจ ARI Clinic,
+                    SUM ( CASE WHEN  ap.clinic = '056' THEN 1 ELSE 0 END ) AS  คลินิกเลิกสุรา,
+                    SUM ( CASE WHEN  ap.clinic = '088' THEN 1 ELSE 0 END ) AS  คลินิกส่องกล้องหลอดลม,
+                    SUM ( CASE WHEN  ap.clinic = '005' THEN 1 ELSE 0 END ) AS  คลินิกหอบหืด Ashma,
+                    SUM ( CASE WHEN  ap.clinic = '079' THEN 1 ELSE 0 END ) AS  คลินิกพยาบาลชุมชน,
+                    SUM ( CASE WHEN  ap.clinic = '075' THEN 1 ELSE 0 END ) AS  คลินิกอาชีวเวชกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '021' THEN 1 ELSE 0 END ) AS  คลินิกอายุรกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '047' THEN 1 ELSE 0 END ) AS  คลินิกเวชกรรมฟื้นฟู,
+                    SUM ( CASE WHEN  ap.clinic = '086' THEN 1 ELSE 0 END ) AS  คลินิกอายุรกรรม ธาลัสซีเมีย,
+                    SUM ( CASE WHEN  ap.clinic = '060' THEN 1 ELSE 0 END ) AS  คลินิกอายุรกรรม14,
+                    SUM ( CASE WHEN  ap.clinic = '009' THEN 1 ELSE 0 END ) AS  คลินิกไตเทียม HD,
+                    SUM ( CASE WHEN  ap.clinic = '085' THEN 1 ELSE 0 END ) AS  ตรวจทั่วไป GP,
+                    SUM ( CASE WHEN  ap.clinic = '058' THEN 1 ELSE 0 END ) AS  ตรวจพิเศษหัวใจและหลอดเลือด,
+                    SUM ( CASE WHEN  ap.clinic = '032' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมหลอดเลือด,
+                    SUM ( CASE WHEN  ap.clinic = '077' THEN 1 ELSE 0 END ) AS  คลินิกโรคปอด,
+                    SUM ( CASE WHEN  ap.clinic = '042' THEN 1 ELSE 0 END ) AS  คลินิกเด็กโรคเลือด,
+                    SUM ( CASE WHEN  ap.clinic = '083' THEN 1 ELSE 0 END ) AS  คลินิกระงับปวด,
+                    SUM ( CASE WHEN  ap.clinic = '049' THEN 1 ELSE 0 END ) AS  คลินิกโรคสะเก็ดเงิน,
+                    SUM ( CASE WHEN  ap.clinic = '073' THEN 1 ELSE 0 END ) AS  SMC กุมารเวชกรรมทั่วไป,
+                    SUM ( CASE WHEN  ap.clinic = '037' THEN 1 ELSE 0 END ) AS  คลินิกตรวจครรภ์,
+                    SUM ( CASE WHEN  ap.clinic = '038' THEN 1 ELSE 0 END ) AS  คลินิกนมแม่,
+                    SUM ( CASE WHEN  ap.clinic = '041' THEN 1 ELSE 0 END ) AS  คลินิกสูติกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '063' THEN 1 ELSE 0 END ) AS  คลินิกหลังคลอด,
+                    SUM ( CASE WHEN  ap.clinic = '057' THEN 1 ELSE 0 END ) AS  คลินิก MMT,
+                    SUM ( CASE WHEN  ap.clinic = '090' THEN 1 ELSE 0 END ) AS  คลินิกโครงการโรคหัวใจเด็ก,
+                    SUM ( CASE WHEN  ap.clinic = '092' THEN 1 ELSE 0 END ) AS  คลินิกตรวจการนอนหลับ PSG,
+                    SUM ( CASE WHEN  ap.clinic = '026' THEN 1 ELSE 0 END ) AS  คลินิกเท้าเบาหวาน,
+                    SUM ( CASE WHEN  ap.clinic = '013' THEN 1 ELSE 0 END ) AS  คลินิกเบาหวานจอตา,
+                    SUM ( CASE WHEN  ap.clinic = '044' THEN 1 ELSE 0 END ) AS  คลินิกภูมิแพ้เด็ก,
+                    SUM ( CASE WHEN  ap.clinic = '059' THEN 1 ELSE 0 END ) AS  คลินิกโรคตับ,
+                    SUM ( CASE WHEN  ap.clinic = '007' THEN 1 ELSE 0 END ) AS  คลินิกโรคไต CKD,
+                    SUM ( CASE WHEN  ap.clinic = '093' THEN 1 ELSE 0 END ) AS  คลินิกสังคมสงเคราะห์,
+                    SUM ( CASE WHEN  ap.clinic = '033' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมกระดูก,
+                    SUM ( CASE WHEN  ap.clinic = '076' THEN 1 ELSE 0 END ) AS  คลินิกทันตกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '034' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมกระดูกข้อเข่าและสะโพก,
+                    SUM ( CASE WHEN  ap.clinic = '067' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคทางเดินอาหารและทั่วไป,
+                    SUM ( CASE WHEN  ap.clinic = '055' THEN 1 ELSE 0 END ) AS  คลินิกยาเสพติด,
+                    SUM ( CASE WHEN  ap.clinic = '019' THEN 1 ELSE 0 END ) AS  คลินิกลานสายตา,
+                    SUM ( CASE WHEN  ap.clinic = '006' THEN 1 ELSE 0 END ) AS  คลินิกวัณโรค TB,
+                    SUM ( CASE WHEN  ap.clinic = '016' THEN 1 ELSE 0 END ) AS  คลินิกวัดแว่น,
+                    SUM ( CASE WHEN  ap.clinic = '027' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '087' THEN 1 ELSE 0 END ) AS  คลินิกส่องกล้องทางเดินอาหาร,
+                    SUM ( CASE WHEN  ap.clinic = '046' THEN 1 ELSE 0 END ) AS  คลินิกหู คอ จมูก,
+                    SUM ( CASE WHEN  ap.clinic = '070' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคสมองและประสาท,
+                    SUM ( CASE WHEN  ap.clinic = '074' THEN 1 ELSE 0 END ) AS  คลินิกประกันสังคม 304,
+                    SUM ( CASE WHEN  ap.clinic = '068' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคหัวใจและทั่วไป,
+                    SUM ( CASE WHEN  ap.clinic = '035' THEN 1 ELSE 0 END ) AS  คลินิกข้อไหล่และเวชศาสตร์การกีฬา,
+                    SUM ( CASE WHEN  ap.clinic = '081' THEN 1 ELSE 0 END ) AS  คลินิกประกันสังคม,
+                    SUM ( CASE WHEN  ap.clinic = '051' THEN 1 ELSE 0 END ) AS  คลินิกฝังเข็ม,
+                    SUM ( CASE WHEN  ap.clinic = '002' THEN 1 ELSE 0 END ) AS  คลินิกความดันโลหิตสูง,
+                    SUM ( CASE WHEN  ap.clinic = '004' THEN 1 ELSE 0 END ) AS  คลินิกปอดอุดกั้นเรื้อรัง COPD,
+                    SUM ( CASE WHEN  ap.clinic = '053' THEN 1 ELSE 0 END ) AS  คลินิกจิตเวช ฝึกพูด,
+                    SUM ( CASE WHEN  ap.clinic = '010' THEN 1 ELSE 0 END ) AS  คลินิกจิตเวชเด็ก,
+                    SUM ( CASE WHEN  ap.clinic = '011' THEN 1 ELSE 0 END ) AS  คลินิกจิตเวชผู้ใหญ่,
+                    SUM ( CASE WHEN  ap.clinic = '024' THEN 1 ELSE 0 END ) AS  คลินิกประสาทอายุรศาสตร์,
+                    SUM ( CASE WHEN  ap.clinic = '080' THEN 1 ELSE 0 END ) AS  คลินิกศาลาไทย,
+                    SUM ( CASE WHEN  ap.clinic = '028' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมตกแต่ง,
+                    SUM ( CASE WHEN  ap.clinic = '030' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมทางเดินปัสสาวะ,
+                    SUM ( CASE WHEN  ap.clinic = '029' THEN 1 ELSE 0 END ) AS  คลินิกศัลยกรรมประสาท,
+                    SUM ( CASE WHEN  ap.clinic = '048' THEN 1 ELSE 0 END ) AS  คลินิกการแพทย์แผนไทย,
+                    SUM ( CASE WHEN  ap.clinic = '054' THEN 1 ELSE 0 END ) AS  คลินิก Harm Reduction,
+                    SUM ( CASE WHEN  ap.clinic = '078' THEN 1 ELSE 0 END ) AS  คลินิกรังสีวิทยา,
+                    SUM ( CASE WHEN  ap.clinic = '045' THEN 1 ELSE 0 END ) AS  คลินิกพัฒนาการและพฤติกรรมเด็ก,
+                    SUM ( CASE WHEN  ap.clinic = '008' THEN 1 ELSE 0 END ) AS  คลินิกล้างไตทางช่องท้อง CAPD,
+                    SUM ( CASE WHEN  ap.clinic = '043' THEN 1 ELSE 0 END ) AS  คลินิกกุมารเวชกรรม,
+                    SUM ( CASE WHEN  ap.clinic = '039' THEN 1 ELSE 0 END ) AS  คลินิกก่อนสมรส,
+                    SUM ( CASE WHEN  ap.clinic = '040' THEN 1 ELSE 0 END ) AS  คลินิกรักษาผู้มีบุตรยากและผ่าตัดเพื่อเจริญพันธุ์,
+                    SUM ( CASE WHEN  ap.clinic = '062' THEN 1 ELSE 0 END ) AS  คลินิกวัยทอง,
+                    SUM ( CASE WHEN  ap.clinic = '064' THEN 1 ELSE 0 END ) AS  คลินิกวางแผนครอบครัว,
+                    SUM ( CASE WHEN  ap.clinic = '065' THEN 1 ELSE 0 END ) AS  คลินิกส่องกล้องขยายปากมดลูก,
+                    SUM ( CASE WHEN  ap.clinic = '069' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมทั่วไป,
+                    SUM ( CASE WHEN  ap.clinic = '072' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคติดเชื้อและทั่วไป,
+                    SUM ( CASE WHEN  ap.clinic = '014' AND ap.an = ''  THEN 1 ELSE 0 END ) AS  คลินิกROP OPD,
+                    SUM ( CASE WHEN  ap.clinic = '014' AND ap.an <> '' THEN 1 ELSE 0 END ) AS  คลินิกROP IPD,
+                    SUM ( CASE WHEN  ap.clinic = '017' THEN 1 ELSE 0 END ) AS  คลินิกกระจกตา,
+                    SUM ( CASE WHEN  ap.clinic = '091' THEN 1 ELSE 0 END ) AS  คลินิกกัญชาทางการแพทย์,
+                    SUM ( CASE WHEN  ap.clinic = '015' THEN 1 ELSE 0 END ) AS  คลินิกจอประสาทตา,
+                    SUM ( CASE WHEN  ap.clinic = '012' THEN 1 ELSE 0 END ) AS  คลินิกจักษุ,
+                    SUM ( CASE WHEN  ap.clinic = '089' THEN 1 ELSE 0 END ) AS  คลินิกตรวจสมรรถภาพทางปอด,
+                    SUM ( CASE WHEN  ap.clinic = '018' THEN 1 ELSE 0 END ) AS  คลินิกต้อหิน,
+                    SUM ( CASE WHEN  ap.clinic = '082' THEN 1 ELSE 0 END ) AS  คลินิกนอกเวลา,
+                    SUM ( CASE WHEN  ap.clinic = '095' THEN 1 ELSE 0 END ) AS  หน่วยเคมีบำบัด,
+                    SUM ( CASE WHEN  ap.clinic = '096' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคเลือด,
+                    SUM ( CASE WHEN  ap.clinic = '071' THEN 1 ELSE 0 END ) AS  SMC อายุรกรรมโรคไต
+
+
+
+
+
+       FROM oapp AS ap
+       INNER JOIN patient as p ON p.hn = ap.hn 
+       INNER JOIN ovst AS o ON o.hn = ap.hn AND ap.nextdate = o.vstdate
+       INNER JOIN clinic AS c ON c.clinic = ap.clinic
+       INNER JOIN kskdepartment AS k ON k.depcode = ap.depcode
+       WHERE o.vstdate BETWEEN '$datepickers' AND '$datepickert'
+       GROUP BY vdate 
+       ORDER BY vdate ASC  ";
+					$result = pg_query($sql);
+					?>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="box">
+								<div class="box-header">
+									<h3 class="box-title co_dep"><?php echo " ข้อมูลช่วงวันที่ ".thaiDatefull($datepickers)." ถึงวันที่ ".thaiDatefull($datepickert)." || ".$cccc; ?> 
+									<small><?php echo " เวลาที่ใช้ในการประมวลผล ".$bm->stop()." วินาที "; ?></small>
+								</h3>
+								<button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal"> Template </button>
+								<button type="" class="btn btn-default pull-right" class="btn btn-info btn-lg" onclick="export_excel()" > Excel </button>
+							</div>
+							<div class="box-body table-responsive"><span class="fcol"> </span>
+								<table id="example1" class="table table-bordered table-striped">
+									<thead>
+										<tr>
+											<?php
+											$i = pg_num_fields($result);
+											for ($j = 0 ; $j < $i ; $j++) {
+												$fieldname = pg_field_name($result, $j);
+												echo '<th>' . $fieldname . '</th>';
+											}
+											?>
+										</tr> 
+									</thead>
+									<tbody>
+										<? $rw=0;
+										while($row_result = pg_fetch_array($result)) 
+										{ 
+											$rw++;
+											?>
+											<tr>
+												<?php
+												for ($j = 0 ; $j < $i ; $j++) {
+													$fieldname = pg_field_name($result, $j);
+													echo '<td>' . $row_result[$fieldname] . '</td>';
+												} 
+												?>
+											</tr>
+											<?php  
+										}
+										?>                                   
+									</tbody>
+								</table>			
+							</div>
+						</div>
+					</div>
+				</div>  
+				<?php 
+			}
+			?>
+		</section>
+	</div>
+	<?php include"config/footer.class.php"; ?>
+	<?php include"config/js.class.php" ?>
+	<script>
+		$(function () {
+			$('#example1').DataTable()
+			$('#example2').DataTable({
+				'paging'      : true,
+				'lengthChange': false,
+				'searching'   : false,
+				'ordering'    : true,
+				'info'        : true,
+				'autoWidth'   : false
+			})
+		})
+	</script>
+	<script type="text/javascript">
+				function export_excel() 
+		{
+			document.location = "export_excel_c.php?send_excel=<?php echo $send_excel; ?>&datepickers=<?php echo $datepickers; ?>&datepickert=<?php echo $datepickert; ?>&cli_dropdown=<?php echo $cli_dropdown; ?>";
+		}
+	</script>
+
+
+</body>
+</html>
