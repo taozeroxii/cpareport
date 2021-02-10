b0910aed294abbcf06a87aecf602a687e44ceeed
unknown <tao32040@gmail.com>
2021-02-10 10:27:45 +0700

    Update form_menu_medical_record.php

--- form_menu_medical_record.php
+++ form_menu_medical_record.php
@@ -40,12 +40,18 @@ $detailmenu = array(
 	M83.0, N70-N98, N99.2-N99.3, 000-099, P54.6, Q50-Q52, R87, S31.4-S37.6, T19.2-T19.3, T83.3, Y76.-, 201.4, Z12.4, 
 	230.1, Z30.3, Z30.5, Z31.1, Z31.2, 732-236, 239.-, Z43.7, Z87.5, Z97.5
 	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0317" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>' ,
-	'- รหัส ICD ที่ใช้ได้กับผู้ป่วย ชายเท่านั้น ได้แก่รหัส B26.0, C60-63, D07.4-D07.6, D17.6, D29.-,D40.E29.-, E89.5, F52.4, 186.1, L29.1, N40-51, Q53-Q55, R86, S31.2-S31.3, Z12.5',
-	'- ใช้รหัส VW,X,Y เป็นรหัสโรคหลัก',
-	'- การให้รหัส S และ T ในผู้ป่วยรายใดต้องให้รหัสสาเหตุภายนอกร่วมด้วยเสมอ (หากมี S หรือ T ต้องมี v,w,x,y ร่วม ถ้าไม่มีดึงมา) ',
-	'- ห้ามใช้รหัส T31.0-T31.9 ซึ่งเป็นรหัสบอกเปอร์เซ็นต์การเกิดแผลไหม้เป็นโรคหลัก ',
-	'- ห้ามใช้รหัส Z470-Z479,Z480-Z489 ร่วมกับ รหัสกลุ่ม S หรือ T',
-	'- เคสที่มีการลงรหัสตามนี้ E14.9, J06.9, D22.9, L02.9, L03.9, R22.9TO7, T14.0 - T14.9, Z34.9 ด้อยคุณภาพให้ดึงมา'
+	'- รหัส ICD ที่ใช้ได้กับผู้ป่วย ชายเท่านั้น ได้แก่รหัส B26.0, C60-63, D07.4-D07.6, D17.6, D29.-,D40.E29.-, E89.5, F52.4, 186.1, L29.1, N40-51, Q53-Q55, R86, S31.2-S31.3, Z12.5
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0318" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>',
+	'- ใช้รหัส VW,X,Y เป็นรหัสโรคหลัก
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0321" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>',
+	'- การให้รหัส S และ T ในผู้ป่วยรายใดต้องให้รหัสสาเหตุภายนอกร่วมด้วยเสมอ (หากมี S หรือ T ต้องมี v,w,x,y ร่วม ถ้าไม่มีดึงมา) 
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0322" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>',
+	'- ห้ามใช้รหัส T31.0-T31.9 ซึ่งเป็นรหัสบอกเปอร์เซ็นต์การเกิดแผลไหม้เป็นโรคหลัก 
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0323" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>',
+	'- ห้ามใช้รหัส Z470-Z479,Z480-Z489 ร่วมกับ รหัสกลุ่ม S หรือ T
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0319" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>',
+	'- เคสที่มีการลงรหัสตามนี้ E14.9, J06.9, D22.9, L02.9, L03.9, R22.9TO7, T14.0 - T14.9, Z34.9 ด้อยคุณภาพให้ดึงมา
+	<br> <br><a href ="./report_form_multipleselect.php?sql=sql_0320" target="_blank"><button class="btn btn-info"> คลิกเปิดหน้าดึงข้อมูล !!</button></a><hr>'
 )
 
 ?>
