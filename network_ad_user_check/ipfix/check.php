130ef276668aaf5867ab4e23c62edee5d673e52e
eaktamp <eaktamp@gmail.com>
2021-07-06 11:40:36 +0700

    ++++

--- network_ad_user_check/ipfix/check.php
+++ network_ad_user_check/ipfix/check.php
@@ -11,25 +11,19 @@ WHERE 1=1
 ORDER BY id ASC ";
 $result = mysqli_query($connect, $query);
 
-/*
-//SWITCH
-$query = " SELECT * 
-FROM network_switch 
+//SW
+$querys = " SELECT * 
+FROM network_check_swicth 
 WHERE 1=1
-AND check_monitor ='Y'
-ORDER BY vlan ASC,num_group ASC ";
-$result = mysqli_query($connect, $query);
-
+ORDER BY id ASC ";
+$results = mysqli_query($connect, $querys);
 
 //UPS
-$query = " SELECT * 
-FROM network_ups 
+$queryu = " SELECT * 
+FROM network_check_ups 
 WHERE 1=1
-AND check_monitor ='Y'
-ORDER BY vlan ASC,num_group ASC ";
-$result = mysqli_query($connect, $query);
-*/
-
+ORDER BY id ASC ";
+$resultu = mysqli_query($connect, $queryu);
 
 ?>
 
@@ -44,206 +38,129 @@ $result = mysqli_query($connect, $query);
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-    <style>
-        h1 {
-            /* color: green; */
-            text-align: center;
-        }
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="st.css">
 
-        div.one {
-            margin-top: 40px;
-            text-align: center;
-        }
-
-        button {
-            margin-top: 10px;
-        }
-
-        body {
-            background-color: #000;
-            /* color: #CCD1D1; */
-        }
-
-        hr {
-            height: 1px;
-            background-color: #ccc;
-            border: none;
-        }
-
-        .hh3 {
-            color: greenyellow;
-        }
-
-        .hh1 {
-            color: #18C0F0;
-        }
-
-        .bb {
-            font-size: 0.8em;
-        }
-
-        .hhh {
-            text-align: center;
-            color: #566573;
-        }
-    </style>
 </head>
 
 <body>
     <h3 class="text-center hhh">CHECK : SERVER | NETWORK | SWITCH | UPS | TEMP | ELECTRIC <h3>
             <div class="container-fluid text-center">
                 <h3 class="text-center hh3">Mode Server .<h3>
-                        <?php
-                        while ($row = mysqli_fetch_array($result)) {
-                                $exp = $row['server_expdate'];
-                            if ($exp > $today) {
-                        ?>
-                                <button class="btn btn-info  col-md-2 bb"><?php echo $row["server_ip"]; ?></button>
-                            <?php
-                            } else {
-                            ?>
-                                <button class="btn btn-danger  col-md-2 bb"><?php echo $row["server_ip"]; ?></button>
-                        <?php
-                            }
-                        }
-                        ?>
-            </div>
-            <hr>
-            <div class="container-fluid text-center">
-                <h3 class="text-center hh3">Mode Switch.<h3>
-                        <?php
-                        while ($row = mysqli_fetch_array($result)) {
+                        <div class="container">
+                            <div class="row">
+                                <?php while ($row = mysqli_fetch_array($result)) {
+                                    $exp = $row['server_expdate'];
+                                    if ($exp > $today) {
+                                ?>
+                                        <div class="col-md-3">
+                                            <div class="square-service-block-aa">
+                                                <a href="#">
+                                                    <div class="ssb-icon-aa"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                    <h2 class="ssb-title-aa"><?php echo $row["server_ip"]; ?></h2>
+                                                    
+                                                </a>
+                                            </div>
+                                        </div>
+                                    <?php
+                                    } else {
+                                    ?>
+                                        <div class="col-md-3">
+                                            <div class="square-service-block-bb">
+                                                <a href="#">
+                                                    <div class="ssb-icon-bb"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                    <h2 class="ssb-title-bb"><?php echo $row["server_ip"]; ?></h2>
+                                                </a>
+                                            </div>
+                                        </div>
 
-                        ?>
+                                <?php
+                                    }
+                                }
+                                ?>
 
-                            <button title="<?php echo $row["ipaddess"]; ?> ว่าง ใช้งานได้ " name="edit" value="<?php echo $row["id"]; ?>" id="<?php echo $row["id"]; ?>" class="btn btn-info  edit_data col-md-2 bb text-left">
-                                <?php echo $row["ipaddess"]; ?>
-                            </button>
-                        <?php
-                        }
+                            </div>
+                        </div>
 
-                        ?>
-            </div>
-            <hr>
-            <div class="container-fluid text-center">
-                <h3 class="text-center hh3">Mode UPS.<h3>
-                        <?php
-                        while ($row = mysqli_fetch_array($result)) {
-                        ?>
-                            <button title="<?php echo $row["ipaddess"]; ?> ว่าง ใช้งานได้ " name="edit" value="<?php echo $row["id"]; ?>" id="<?php echo $row["id"]; ?>" class="btn btn-info  edit_data col-md-2 bb  text-left">
-                                <?php echo $row["ipaddess"]; ?>
-                            </button>
-                        <?php
-                        }
-                        ?>
-            </div>
+                        <!--  -->
+
+                        <hr>
+                        <div class="container-fluid text-center">
+                            <h3 class="text-center hh3">Mode Switch.<h3>
+                                    <div class="container">
+                                        <div class="row">
+                                            <?php while ($rows = mysqli_fetch_array($results)) {
+                                                $exp = $rows['sw_expdate'];
+                                                if ($exp > $today) {
+                                            ?>
+                                                    <div class="col-md-3">
+                                                        <div class="square-service-block-cc">
+                                                            <a href="#">
+                                                                <div class="ssb-icon-cc"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                                <h2 class="ssb-title-cc"><?php echo $rows["sw_name"]; ?></h2>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                <?php
+                                                } else {
+                                                ?>
+                                                    <div class="col-md-3">
+                                                        <div class="square-service-block-dd">
+                                                            <a href="#">
+                                                                <div class="ssb-icon-dd"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                                <h2 class="ssb-title-dd"><?php echo $rows["sw_name"]; ?></h2>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+
+                                        </div>
+                                    </div>
+                        </div>
+                        <hr>
+                        <div class="container-fluid text-center">
+                            <h3 class="text-center hh3">Mode UPS.<h3>
+                                    <div class="container">
+                                        <div class="row">
+                                            <?php while ($rowu = mysqli_fetch_array($resultu)) {
+                                                $exp = $rowu['ups_expdate'];
+                                                if ($exp > $today) {
+                                            ?>
+                                                    <div class="col-md-3">
+                                                        <div class="square-service-block-ee">
+                                                            <a href="#">
+                                                                <div class="ssb-icon-ee"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                                <h2 class="ssb-title-ee"><?php echo $rowu["ups_name"]; ?></h2>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+                                                <?php
+                                                } else {
+                                                ?>
+                                                    <div class="col-md-3">
+                                                        <div class="square-service-block-ff">
+                                                            <a href="#">
+                                                                <div class="ssb-icon-ff"><i class="fa fa-desktop" aria-hidden="true"></i></div>
+                                                                <h2 class="ssb-title-ff"><?php echo $rowu["ups_name"]; ?></h2>
+                                                            </a>
+                                                        </div>
+                                                    </div>
+
+                                            <?php
+                                                }
+                                            }
+                                            ?>
+
+                                        </div>
+                                    </div>
+                        </div>
 
 </body>
 
 </html>
 
 
-
-
-<div id="add_data_Modal" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <!-- <button type="button" class="close" data-dismiss="modal">&times;</button> -->
-                <h4 class="modal-title">ผลการตรวจสอบระบบ</h4>
-                <h6><?php echo $today; ?></h6>
-            </div>
-            <div class="modal-body">
-                <form method="post" id="insert_form">
-                    <label>สถานะตรวจเช็ค</label>
-                    <input type="text" name="ipaddess" id="ipaddess" class="form-control" readonly />
-                    <br />
-                    <label>หมายเหตุ</label>
-                    <textarea name="note" id="note" class="form-control"></textarea>
-                    <br />
-                    <input type="hidden" name="employee_id" id="employee_id" />
-                    <input type="submit" name="insert" id="insert" value="Insert" class="btn btn-success btn-lg btn-block" />
-                </form>
-            </div>
-            <!-- <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            </div> -->
-        </div>
-    </div>
-</div>
-<script>
-    $(document).ready(function() {
-        $('#add').click(function() {
-            $('#insert').val("Insert");
-            $('#insert_form')[0].reset();
-        });
-        $(document).on('click', '.edit_data', function() {
-            var employee_id = $(this).attr("id");
-            $.ajax({
-                url: "fetch.php",
-                method: "POST",
-                data: {
-                    employee_id: employee_id
-                },
-                dataType: "json",
-                success: function(data) {
-                    $('#ipaddess').val(data.ipaddess);
-                    $('#updatedate').val(data.updatedate);
-                    $('#telephone').val(data.telephone);
-                    $('#location').val(data.location);
-                    $('#note').val(data.note);
-                    $('#employee_id').val(data.id);
-                    $('#insert').val("Update");
-                    $('#add_data_Modal').modal('show');
-                }
-
-            });
-        });
-        $('#insert_form').on("submit", function(event) {
-            event.preventDefault();
-            if ($('#ipaddess').val() == "") {
-                alert("ipaddess is required");
-                //  } else if ($('#updatedate').val() == '') {
-                //      alert("updatedate is required");
-            } else if ($('#telephone').val() == '') {
-                alert("telephone is required");
-            } else if ($('#location').val() == '') {
-                alert("location is required");
-            } else if ($('#note').val() == '') {
-                alert("note is required");
-            } else {
-                $.ajax({
-                    url: "insert.php",
-                    method: "POST",
-                    data: $('#insert_form').serialize(),
-                    beforeSend: function() {
-                        $('#insert').val("Inserting");
-                    },
-                    success: function(data) {
-                        $('#insert_form')[0].reset();
-                        $('#add_data_Modal').modal('hide');
-                        $('#employee_table').html(data);
-                        location.reload();
-                    }
-                });
-            }
-        });
-        $(document).on('click', '.view_data', function() {
-            var employee_id = $(this).attr("id");
-            if (employee_id != '') {
-                $.ajax({
-                    url: "select.php",
-                    method: "POST",
-                    data: {
-                        employee_id: employee_id
-                    },
-                    success: function(data) {
-                        $('#employee_detail').html(data);
-                        $('#dataModal').modal('show');
-
-                    }
-                });
-            }
-        });
-    });
-</script>
