854119a283d0e9b0905aa07331ed6ff5990b5736
NewbieTao <39129447+taozeroxii@users.noreply.github.com>
2022-03-16 19:16:36 -0700

    hr-salary

--- gsb/uploadfile/upload.php
+++ gsb/uploadfile/upload.php
@@ -2,7 +2,8 @@
 if(!empty($_FILES)) {
 if(is_uploaded_file($_FILES['userImage']['tmp_name'])) {
 $sourcePath = $_FILES['userImage']['tmp_name'];
-$targetPath = "D:/Ampps/www/report/gsb/excelfile/".$_FILES['userImage']['name'];
+$targetPath = "C:/Program Files/Ampps/www/report/gsb/excelfile/".$_FILES['userImage']['name'];
+
 if(move_uploaded_file($sourcePath,$targetPath)) {
 ?>
 <!-- <img src="<?php // echo $targetPath; ?>" width="100px" height="100px" /> -->
